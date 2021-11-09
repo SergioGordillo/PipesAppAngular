eb62a919b9309f8f40eb1b53486dc383415f05bc
Sergio Gordillo <sergiogordillodev@gmail.com>
2021-11-10 00:08:16 +0100

    feat: string and date pipes implemented

--- src/app/sales/pages/basics/basics.component.ts
+++ src/app/sales/pages/basics/basics.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-basics',
@@ -6,11 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BasicsComponent implements OnInit {
+export class BasicsComponent {
 
-  constructor() { }
+  name: string = "Sergio"
+  nameUpper: string ="SERGIO";
+  nameLower: string ="sergio";
 
-  ngOnInit(): void {
-  }
+  date: Date = new Date(); //today's day
+  
 
 }
