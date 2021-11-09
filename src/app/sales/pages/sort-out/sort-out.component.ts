2372e94d14e7e19f07cdc2c21a068188f0225552
Sergio Gordillo <sergiogordillodev@gmail.com>
2021-11-09 14:34:15 +0100

    feat: Routes and Menu implemented

---
+++ src/app/sales/pages/sort-out/sort-out.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sort-out',
+  templateUrl: './sort-out.component.html',
+  styles: [
+  ]
+})
+export class SortOutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -15,27 +15,28 @@ export class MenuComponent implements OnInit {
 
   ngOnInit(): void {
     this.items = [
-      {
-          label: 'File',
-          items: [{
-                  label: 'New', 
-                  icon: 'pi pi-fw pi-plus',
-                  items: [
-                      {label: 'Project'},
-                      {label: 'Other'},
-                  ]
-              },
-              {label: 'Open'},
-              {label: 'Quit'}
-          ]
+    {
+      label: "Angular Pipes",
+      icon: "pi pi-desktop",
+      items: [{
+        label: "Texts and Dates",
+        icon: "pi pi-align-left",
+        routerLink: '/'
       },
       {
-          label: 'Edit',
-          icon: 'pi pi-fw pi-pencil',
-          items: [
-              {label: 'Delete', icon: 'pi pi-fw pi-trash'},
-              {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
-          ]
+        label: "Numbers",
+        icon: "pi pi-dollar",
+        routerLink: 'numbers'
+      },
+      {
+        label: "No commons",
+        icon: "pi pi-globe",
+        routerLink: 'no-commons'
+      }]},
+  { 
+    label: "Customized Pipes",
+    icon: "pi pi-cog",
+
       }
   ];
   }
