ff5685b9b53ec1494a1ca86dbb25d13f4afdbc76
Sergio Gordillo <sergiogordillodev@gmail.com>
2021-11-16 00:02:26 +0100

    feat: i18n Plural Pipe implemented

--- src/app/sales/pages/no-commons/no-commons.component.ts
+++ src/app/sales/pages/no-commons/no-commons.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 })
 export class NoCommonsComponent {
 
+  // i18n Select
   name : string = "Sergio";
   gender: string = "masculino";
 
@@ -15,6 +16,17 @@ export class NoCommonsComponent {
     "masculino": "invitarlo",
     "femenino": "invitarla"
   }
+
+   // i18n Plural
+
+   customers: string[] =["María", "Pedro", "Jorge", "Ramón"];
+
+   customersMap={
+     '=0': "No tenemos ningún cliente esperando.",
+     '=1': "Tenemos un cliente esperando.",
+     '=2': "Tenemos dos clientes esperando.",
+     "other": "Tenemos # clientes esperando"
+   }
  
 
 }
